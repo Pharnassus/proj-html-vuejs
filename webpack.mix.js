d114ab2c8e5149154c7845f62716e8d6d401b74b
Marian <marian.corlade@outlook.it>
2021-01-13 22:01:52 +0100

    main -section 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.sass('src/app.scss', 'css');
+
+mix.sass('src/app.scss', 'dist').options({
+    processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
+});
